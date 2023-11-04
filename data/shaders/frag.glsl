0ec600dfa67fd504ebe2dde9ca5d96a2b66622fe
Jens Kromdijk <jenskromdijk@gmail.com>
2023-11-04 18:08:45 +0000

    shade

--- data/shaders/frag.glsl
+++ data/shaders/frag.glsl
@@ -8,7 +8,20 @@ in vec2 uvs;
 out vec4 f_color;
 
 void main() {
-    //vec2 texCoords = vec2(uvs.x)
-    vec3 mult = sin(texture(noise, uvs + (time * 0.0001)).rgb + time * 0.01);
+    //vec2 texCoords = vec2(uvs.x + sin(time * 0.01 + uvs.y) * 0.01, uvs.y + sin(time * 0.01 + uvs.x) * 0.01);
+    vec3 mult = texture(noise, uvs - (time * 0.0001)).rgb;
+    mult.rgb += sin(mult.rgb * 60 + 100 * sin(time * 0.0000001)) * 0.1;
+    if (mult.r < 0.2) {
+        vec3 color = vec3(0.4, 0.4, 0.0);
+        mult.rgb = color;
+    }
+    else if (mult.r < 0.4) {
+        vec3 color = vec3(0.4, 0.0, 0.0);
+        mult.rgb = color;
+    }
+    else if (mult.r < 0.8) {
+        vec3 color = vec3(0.0, 0.0, 0.0);
+        mult.rgb = color;
+    }
     f_color = vec4(texture(tex, uvs).rgb + mult, 1.0);
 }
